f74c22c75f05c39f6340a077b232bd0cb12c86cd
Flávio Félix <fau_felix30@hotmail.com>
2020-06-15 15:12:40 -0300

    Criando um programa de cálculo de média dos alunos

---
+++ works/aulajs/index.js
@@ -0,0 +1,17 @@
+// Criar um programa que calcula a média
+// das notas entre os alunos e envia
+// mensagem do cálculo de média
+
+const aluno01 = "flavio"
+const aluno02 = 'Diego'
+const aluno03 = 'Fulano'
+const notaAluno01 = 9.8
+const notaAluno02 = 10
+const notaAluno03 = 2
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03)/3
+
+console.log(media)
+
+
+
