a86f60bf8c5e2bd6735575bf42e68f019912d297
Flávio Félix <fau_felix30@hotmail.com>
2020-06-15 16:01:38 -0300

    Feito o desafio 1

--- works/aulajs/operadores.js
+++ works/aulajs/operadores.js
@@ -12,7 +12,21 @@
 
 ===================================== */
 
-console.log(4 == "4")// true
-console.log(4 === "4")// false
-console.log(4 != "5")// true
-console.log(4 !== "5")// true
+// DESAFIO 1
+const idade = 17
+// Verificar se a pessoa é maior ou igual a  18 anos
+
+// se sim, deixar entrar, se não, bloquear a entrada
+if(idade >= 18) {
+  console.log('Deixar entrar')
+}else{
+  console.log('Bloquear a entrada')
+}
+// se a pessoa tiver 17 anos
+// avisar para voltar quando fizer 18 anos
+if(idade === 17){
+  console.log('Volte quando tiver 18 anos')
+
+}
+
+
