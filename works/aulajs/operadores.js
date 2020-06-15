e29360ec216cca5be6059743381a059e00d269cf
Flávio Félix <fau_felix30@hotmail.com>
2020-06-15 15:42:01 -0300

    Estudando operadores de comparação maior ou menor

---
+++ works/aulajs/operadores.js
@@ -0,0 +1,18 @@
+/*====================================
+  OPERADORES DE COMPARAÇÃO
+
+  >   Maior
+  <   Menor
+  >=  Maior igual a
+  <=  Menor igual a
+  ==  Igual a 
+  === Igual e do mesmo tipo
+  !=  Diferente de
+  !== Diferente, inclusive do tipo 
+
+===================================== */
+
+console.log(5 > 4)// true
+console.log(5 < 4)// false
+console.log(5 >= 4)// true
+console.log(4 <= 4)// true
