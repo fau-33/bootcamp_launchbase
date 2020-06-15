eeda76b235aa35ab7f5a0e57ddb793e0a3e3ec48
Flávio Félix <fau_felix30@hotmail.com>
2020-06-15 15:47:35 -0300

    Estudando operadores de comparação com igual =

--- works/aulajs/operadores.js
+++ works/aulajs/operadores.js
@@ -12,7 +12,7 @@
 
 ===================================== */
 
-console.log(5 > 4)// true
-console.log(5 < 4)// false
-console.log(5 >= 4)// true
-console.log(4 <= 4)// true
+console.log(4 == "4")// true
+console.log(4 === "4")// false
+console.log(4 != "5")// true
+console.log(4 !== "5")// true
